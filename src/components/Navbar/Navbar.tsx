928195667c800635aedd22e4100535cf9d7b8074
Rakshit Arora <rakshit087@gmail.com>
2022-12-17 18:16:39 +0530

    updated navbar, added logout and avatars

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,55 +1,123 @@
-import { Button, Modal } from "antd";
-import { Header } from "antd/es/layout/layout";
-import React, { useContext, useState } from "react"
+import { Modal } from "antd";
+import {
+  Avatar,
+  Button,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+} from "@chakra-ui/react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../../context/AppContext";
 import { NetworkType } from "../../models";
+import { Flex, Text } from "@chakra-ui/react";
 
 const Navbar = () => {
+  const context = useContext(AppContext);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const connectWallet = (network: NetworkType) => {
+    context?.connectWallet(network);
+    setIsModalOpen(false);
+  };
+  if (context == null) return <h1>ERROR</h1>;
 
-    const context = useContext(AppContext);
+  return (
+    <>
+      <Modal open={isModalOpen}>
+        <div className="text-center">
+          <h3 className="text-3xl font-semibold text-[#69248A]">
+            Connect to Orbis
+          </h3>
+          <p className="my-3 text-xl">With your wallet</p>
+          <hr />
+        </div>
+        <div className="flex flex-col items-center justify-center mt-4">
+          <Button
+            className="mb-4 bg-[#B45008] text-white w-44 "
+            onClick={() => connectWallet(NetworkType.Ethereum)}
+          >
+            Metamask
+          </Button>
+          <Button
+            className="bg-[#201191] text-white  w-44 "
+            onClick={() => connectWallet(NetworkType.Solana)}
+          >
+            Phantom
+          </Button>
+        </div>
+      </Modal>
+      <Flex
+        w={"full"}
+        h={20}
+        bgColor={"#69248A"}
+        position="fixed"
+        px={16}
+        top={0}
+        zIndex={1}
+        justify={"space-between"}
+        align={"center"}
+      >
+        <Flex align={"center"}>
+          {/* Community data */}
+          {context.groupDetails?.content.pfp ? (
+            <Avatar
+              src={context.groupDetails?.content.pfp}
+              name="group pfp"
+              mr={8}
+            />
+          ) : null}
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const openModal = () => {
-        setIsModalOpen(true)
-    }
-
-    const connectWallet = (network: NetworkType) => {
-        context?.connectWallet(network);
-        setIsModalOpen(false)
-    }
-
-    
-    if(context == null) return <h1>ERROR</h1>
-    
-    return (
-        <>
-        <Modal open={isModalOpen}  >
-            <div className="text-center">
-                <h3 className="text-3xl font-semibold text-[#69248A]">Connect to Orbis</h3>
-                <p className="my-3 text-xl">With your wallet</p>
-                <hr />
-            </div>
-            <div className="flex flex-col justify-center items-center mt-4">
-                <Button className="mb-4 bg-[#B45008] text-white w-44 " onClick={() => connectWallet(NetworkType.Ethereum)}>Metamask</Button>
-                <Button className="bg-[#201191] text-white  w-44 " onClick={() => connectWallet(NetworkType.Solana)}>Phantom</Button>
-            </div>
-        </Modal>
-        <Header style={{ position: 'sticky', top: 0, zIndex: 1, width: '100%' }} className="p-4 bg-[#69248A] text-white">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="flex justify-between items-center ">
-                    {/* Community data */}
-                    {context.groupDetails?.content.pfp ? <img src={context.groupDetails?.content.pfp} className="max-h-12 mr-10 rounded-full"/> : null}
-
-                    <h2>{context.groupDetails?.content.name ? context.groupDetails.content.name : null}</h2>
-                </div>
-                <div>
-                    {context.connectedAddress ? <Button className="text-white" disabled><span className="text-white">{context.connectedAddress.slice(0,8) + `...`}</span></Button> : <Button type="default" className="text-white" onClick={openModal}>Connect Wallet</Button>}
-                </div>
-            </div>
-        </Header>
-        </>
-    )
-}
+          <Text color={"white"} fontSize={"xl"}>
+            {context.groupDetails?.content.name
+              ? context.groupDetails.content.name.toUpperCase()
+              : null}
+          </Text>
+        </Flex>
+        <div>
+          {context.connectedAddress ? (
+            // <Button className="text-white" disabled>
+            //   <span className="text-white">
+            //
+            //   </span>
+            // </Button>
+            <Popover>
+              <PopoverTrigger>
+                <Avatar size={"sm"} />
+              </PopoverTrigger>
+              <PopoverContent width={64}>
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>
+                  {context.connectedAddress.slice(0, 8) + `...`}
+                </PopoverHeader>
+                <PopoverBody textAlign={"center"}>
+                  <Button
+                    size={"sm"}
+                    onClick={() => {
+                      context.logout();
+                    }}
+                  >
+                    {" "}
+                    Log Out{" "}
+                  </Button>
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
+          ) : (
+            <Button onClick={openModal} size={"sm"}>
+              Connect Wallet
+            </Button>
+          )}
+        </div>
+      </Flex>
+    </>
+  );
+};
 
 export default Navbar;
