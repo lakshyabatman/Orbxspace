ab955324153b3ed95fdd44d680f11c0da33203eb
Rakshit Arora <rakshit087@gmail.com>
2022-12-17 18:29:51 +0530

    seperate component for useravatar

---
+++ src/components/Navbar/UserAvatar.tsx
@@ -0,0 +1,44 @@
+import {
+  Avatar,
+  Button,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+} from "@chakra-ui/react";
+import React from "react";
+
+interface UserAvatarProps {
+  connectedAddress: string;
+  logout: () => void;
+  connectWallet: () => void;
+}
+
+export const UserAvatar = ({ connectedAddress, logout }: UserAvatarProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Avatar size={"sm"} />
+      </PopoverTrigger>
+      <PopoverContent width={64}>
+        <PopoverArrow />
+        <PopoverCloseButton />
+        <PopoverHeader>{connectedAddress.slice(0, 8) + `...`}</PopoverHeader>
+        <PopoverBody textAlign={"center"}>
+          <Button
+            size={"sm"}
+            onClick={() => {
+              logout();
+            }}
+          >
+            {" "}
+            Log Out{" "}
+          </Button>
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
+  );
+};
