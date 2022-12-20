39273ac5580dd7c528ca7e9b888802a674dbf18c
Rakshit Arora <rakshit087@gmail.com>
2022-12-20 16:43:24 +0530

    added replies icon

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,5 +1,7 @@
-import { Box } from "@chakra-ui/react";
 import React from "react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { ChatIcon } from "@chakra-ui/icons";
+import { IconButton } from "@chakra-ui/react";
 
 interface PostProps {
   postContent: string;
@@ -10,7 +12,13 @@ interface PostProps {
 export const Post = ({ postContent, postAuthor, postDate }: PostProps) => {
   return (
     <Box w={"full"} bgColor={"white"} mb={4} rounded={"md"} p={4}>
-      <h1>{postContent}</h1>
+      <Flex justify={"space-between"}>
+        <Text w={"75%"}>{postContent}</Text>
+        <Flex align={"center"}>
+          <ChatIcon mr={2} />
+          <Text>10 Replies</Text>
+        </Flex>
+      </Flex>
     </Box>
   );
 };
