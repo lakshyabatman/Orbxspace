cc4a2c266e0591305e120fde9d3f1443b8da97b0
Rakshit Arora <rakshit087@gmail.com>
2022-12-24 03:39:36 +0530

    made focus color to purple

--- src/components/PostDetailCard/PostDetailCard.tsx
+++ src/components/PostDetailCard/PostDetailCard.tsx
@@ -1,41 +1,49 @@
-import { ChatIcon } from '@chakra-ui/icons'
-import { Avatar, Flex } from '@chakra-ui/react'
-import React from 'react'
-import { Post, ReactionType } from '../../models'
-import { unixToAgo } from '../../utilities/unixToAgo'
-import ReactionSection from '../ReactionSection/ReactionSection'
-
-
+import { ChatIcon } from "@chakra-ui/icons";
+import { Avatar, Flex } from "@chakra-ui/react";
+import React from "react";
+import { Post, ReactionType } from "../../models";
+import { unixToAgo } from "../../utilities/unixToAgo";
+import ReactionSection from "../ReactionSection/ReactionSection";
 
 export interface PostDetailCardProps {
-    currentPost: Post,
-    react: (reactioonType: ReactionType) => Promise<boolean>
+  currentPost: Post;
+  react: (reactioonType: ReactionType) => Promise<boolean>;
 }
 
-const PostDetailCard:React.FC<PostDetailCardProps> = ({currentPost,react}) => {
-    
+const PostDetailCard: React.FC<PostDetailCardProps> = ({
+  currentPost,
+  react,
+}) => {
   return (
-   <div>
-
-    <div className="flex items-center">
-            <Avatar size={"sm"} mr={3} src={currentPost.creator_details.profile.pfp} />
-            <span className="text-md mr-3">
-              {currentPost.creator_details.profile.username}
-            </span>
-            <span className="text-md text-gray-500">
-              {unixToAgo(currentPost.timestamp)}
-            </span>
-    </div >
+    <div>
+      <div className="flex items-center">
+        <Avatar
+          size={"sm"}
+          mr={3}
+          src={currentPost.creator_details.profile.pfp}
+        />
+        <span className="mr-3 text-md">
+          {currentPost.creator_details.profile.username}
+        </span>
+        <span className="text-gray-500 text-md">
+          {unixToAgo(currentPost.timestamp)}
+        </span>
+      </div>
       <h1 className="my-4">{currentPost?.content.body}</h1>
-    <div className="flex justify-between mb-4">
+      <div className="flex justify-between mb-4">
         <Flex align={"center"}>
-            <ChatIcon mr={2} />
-            <span>{currentPost.count_replies} Replies</span>
+          <ChatIcon mr={2} />
+          <span>{currentPost.count_replies} Replies</span>
         </Flex>
-        <ReactionSection heartCounts={currentPost.count_likes} downvoteCounts={currentPost.count_downvotes} hahaCounts={currentPost.count_haha} react={react} />
-     </div>
-   </div>
-  )
-}
+        <ReactionSection
+          heartCounts={currentPost.count_likes}
+          downvoteCounts={currentPost.count_downvotes}
+          hahaCounts={currentPost.count_haha}
+          react={react}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default PostDetailCard
+export default PostDetailCard;
--- src/components/pages/PostPage/PostPage.tsx
+++ src/components/pages/PostPage/PostPage.tsx
@@ -1,5 +1,5 @@
 import { Box, FormControl, Input, Textarea } from "@chakra-ui/react";
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../../context/AppContext";
 
 import { PostWithComments, ReactionType } from "../../../models";
@@ -7,64 +7,68 @@ import PostDetailCard from "../../PostDetailCard/PostDetailCard";
 
 const PostPage = () => {
   const context = useContext(AppContext);
-
-  const [comments, setComments] = useState<PostWithComments[]>([])
-  const [comment,setComment] = useState("");
-
+  const [comments, setComments] = useState<PostWithComments[]>([]);
+  const [comment, setComment] = useState("");
 
   if (context == null) return <h1>ERROR</h1>;
+  const { currentPost, currentPostComments } = context;
+  if (currentPost == null) return <h1>ERROR</h1>;
 
-
-  const {currentPost, currentPostComments} = context;
-
-  if(currentPost == null) return <h1>ERROR</h1>
-
-  const reactToPost = async (reaction: ReactionType) : Promise<boolean> => {
+  const reactToPost = async (reaction: ReactionType): Promise<boolean> => {
     return await context.reactToPost(currentPost.stream_id, reaction);
-
-  }
-
+  };
 
   useEffect(() => {
-    const nestedComments: Record<string, PostWithComments> ={};
-    const isComment: Record<string, boolean> = {}
+    const nestedComments: Record<string, PostWithComments> = {};
+    const isComment: Record<string, boolean> = {};
 
-    currentPostComments.forEach(comment => {
-      if(comment.reply_to && nestedComments[comment.reply_to]) {
-        nestedComments[comment.reply_to].comments.push({...comment, comments:[]});
-        nestedComments[comment.stream_id] = {...comment, comments:[]};
-        isComment[comment.stream_id] = false
-      }else {
-        nestedComments[comment.stream_id] = {...comment, comments:[]};
-        isComment[comment.stream_id] = true
+    currentPostComments.forEach((comment) => {
+      if (comment.reply_to && nestedComments[comment.reply_to]) {
+        nestedComments[comment.reply_to].comments.push({
+          ...comment,
+          comments: [],
+        });
+        nestedComments[comment.stream_id] = { ...comment, comments: [] };
+        isComment[comment.stream_id] = false;
+      } else {
+        nestedComments[comment.stream_id] = { ...comment, comments: [] };
+        isComment[comment.stream_id] = true;
       }
-    })
+    });
 
-    const onlyMasterComments = Object.keys(nestedComments).filter(comment => isComment[comment]).map(commentKey => nestedComments[commentKey]);
-    setComments(onlyMasterComments)
-  }, [currentPostComments])
-
+    const onlyMasterComments = Object.keys(nestedComments)
+      .filter((comment) => isComment[comment])
+      .map((commentKey) => nestedComments[commentKey]);
+    setComments(onlyMasterComments);
+  }, [currentPostComments]);
 
   const addComment = async (event: any) => {
-    if(event.keyCode == 13) {
-
-      await context.createComment(comment, currentPost.stream_id, null, [])
+    if (event.keyCode == 13) {
+      await context.createComment(comment, currentPost.stream_id, null, []);
     }
-  }
+  };
 
   return (
     <Box className="mx-10">
-      <PostDetailCard currentPost={currentPost} react={reactToPost}/>
-      <hr/>
-      <FormControl onKeyDown={addComment} >
+      <PostDetailCard currentPost={currentPost} react={reactToPost} />
+      <hr />
+      <FormControl onKeyDown={addComment} mt={6}>
         {/* <FormLabel>Email address</FormLabel> */}
-        <Input type='text' value={comment} onChange={(e) => setComment(e.target.value)} required={true}/>
+        <Box bgColor={"#EDEDED"} p={3}>
+          <Input
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            required={true}
+            placeholder="Add a reply"
+            bgColor={"white"}
+            focusBorderColor={"#69248A"}
+          />
+        </Box>
         {/* <FormHelperText>We'll never share your email.</FormHelperText> */}
       </FormControl>
-
-
     </Box>
-  )
-}
+  );
+};
 
-export default PostPage
+export default PostPage;
