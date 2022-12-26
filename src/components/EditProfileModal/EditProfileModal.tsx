4ab6ce157d5e08c819d796d678c6b797087bef6f
Rakshit Arora <rakshit087@gmail.com>
2022-12-27 04:06:12 +0530

    added edit profile modal

---
+++ src/components/EditProfileModal/EditProfileModal.tsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  Input,
+  Flex,
+  Textarea,
+} from "@chakra-ui/react";
+
+import { ChannelType } from "../../models";
+
+export interface CreateChannelModalProps {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+export const EditProfileModal: React.FC<CreateChannelModalProps> = ({
+  isOpen,
+  onOpen,
+  onClose,
+}) => {
+  const [linkToPfp, setLinkToPfp] = useState("");
+  const [userName, setUserName] = useState("");
+  const [bio, setBio] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size={"lg"}>
+      <ModalOverlay backdropFilter="blur(10px)" />
+      <ModalContent>
+        <ModalBody py={8}>
+          <Box textAlign={"center"} mb={8}>
+            <Text fontSize={"2xl"} color={"#69248A"} fontWeight={"semibold"}>
+              Edit Profile
+            </Text>
+          </Box>
+          <Text mb={1} fontSize={"xs"} fontWeight={"semibold"}>
+            LINK TO PFP
+          </Text>
+          <Flex alignItems={"center"} mb={8}>
+            <Input
+              placeholder="Add pfp link"
+              value={linkToPfp}
+              onChange={(e) => setLinkToPfp(e.target.value)}
+              rounded={"sm"}
+              boxShadow={"0px 0px 20px 1px rgba(175, 92, 214, 0.25)"}
+              focusBorderColor={"#69248A"}
+              mr={4}
+            />
+          </Flex>
+          <Text mb={1} fontSize={"xs"} fontWeight={"semibold"}>
+            USERNAME
+          </Text>
+          <Flex alignItems={"center"} mb={8}>
+            <Input
+              placeholder="Add username"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              rounded={"sm"}
+              boxShadow={"0px 0px 20px 1px rgba(175, 92, 214, 0.25)"}
+              focusBorderColor={"#69248A"}
+              mr={4}
+            />
+          </Flex>
+          <Text mb={1} fontSize={"xs"} fontWeight={"semibold"}>
+            BIO
+          </Text>
+          <Flex alignItems={"center"} mb={8}>
+            <Textarea
+              placeholder="Add bio"
+              value={bio}
+              onChange={(e) => setBio(e.target.value)}
+              rounded={"sm"}
+              boxShadow={"0px 0px 20px 1px rgba(175, 92, 214, 0.25)"}
+              focusBorderColor={"#69248A"}
+              mr={4}
+              resize={"none"}
+              height={48}
+            />
+          </Flex>
+
+          <Button
+            isLoading={loading}
+            display={"block"}
+            mx={"auto"}
+            bg={
+              "linear-gradient(90deg, rgba(239, 136, 210, 1), rgba(175, 92, 214, 1))"
+            }
+            color={"white"}
+            rounded={"md"}
+            w={48}
+            h={10}
+            _hover={{
+              bg: "linear-gradient(90deg, rgba(239, 136, 210, 1), rgba(175, 92, 214, 1))",
+            }}
+            _active={{
+              bg: "linear-gradient(90deg, rgba(239, 136, 210, 1), rgba(175, 92, 214, 1))",
+            }}
+          >
+            Save Changes
+          </Button>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
--- src/components/Navbar/UserAvatar.tsx
+++ src/components/Navbar/UserAvatar.tsx
@@ -8,8 +8,11 @@ import {
   PopoverContent,
   PopoverHeader,
   PopoverTrigger,
+  useDisclosure,
 } from "@chakra-ui/react";
 import React from "react";
+
+import { EditProfileModal } from "../EditProfileModal/EditProfileModal";
 
 interface UserAvatarProps {
   connectedAddress: string;
@@ -17,37 +20,36 @@ interface UserAvatarProps {
 }
 
 export const UserAvatar = ({ connectedAddress, logout }: UserAvatarProps) => {
-  return (
-    <Popover>
-      <PopoverTrigger>
-        <Avatar size={"sm"} />
-      </PopoverTrigger>
-      <PopoverContent width={64}>
-        <PopoverArrow />
-        <PopoverCloseButton />
-        <PopoverHeader>{connectedAddress.slice(0, 8) + `...`}</PopoverHeader>
-        <PopoverBody textAlign={"center"}>
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-          <div className="flex flex-col items-center">
-          <Button
-              className="mb-2"
-              size={"sm"}
-              onClick={() => console.log("hu")}
-            >
-              <>Update Profile</>
-            </Button>
-            <Button
-              size={"sm"}
-              onClick={() => {
-                logout();
-              }}
-            >
-              {" "}
-              Log Out{" "}
-            </Button>
-          </div>
-        </PopoverBody>
-      </PopoverContent>
-    </Popover>
+  return (
+    <>
+      <EditProfileModal isOpen={isOpen} onClose={onClose} onOpen={onOpen} />
+      <Popover>
+        <PopoverTrigger>
+          <Avatar size={"sm"} />
+        </PopoverTrigger>
+        <PopoverContent width={64}>
+          <PopoverArrow />
+          <PopoverCloseButton />
+          <PopoverHeader>{connectedAddress.slice(0, 8) + `...`}</PopoverHeader>
+          <PopoverBody textAlign={"center"}>
+            <div className="flex flex-col items-center">
+              <Button className="mb-2" size={"sm"} onClick={() => onOpen()}>
+                <>Update Profile</>
+              </Button>
+              <Button
+                size={"sm"}
+                onClick={() => {
+                  logout();
+                }}
+              >
+                Log Out
+              </Button>
+            </div>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+    </>
   );
 };
