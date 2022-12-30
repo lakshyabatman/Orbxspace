6cfc9052c8bcaeb1419242fe3654874c8ad3547f
Rakshit Arora <rakshit087@gmail.com>
2022-12-30 23:04:02 +0530

    removed chakra from layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,21 +1,20 @@
 import React, { PropsWithChildren } from "react";
 import Navbar from "../Navbar/Navbar";
 import SideMenu from "../SideMenu/SideMenu";
-import { Box, Flex } from "@chakra-ui/react";
 
 const LayoutPage: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <Flex flexDir={"column"} className="min-h-screen">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div  className="min-h-screen  grid grid-cols-8  bg-[#fcf6ff] px-12">
-        <Box  w={"full"} className="2xl:col-span-3 lg:col-span-2 col-span-3">
-        <SideMenu />
-        </Box>
-        <Box mt={28} w={"full"} className="col-span-5 xl:col-span-4 2xl:col-span-3">
-            {children}
-        </Box>
+      <div className="min-h-screen  grid grid-cols-8  bg-[#fcf6ff] px-12">
+        <div className="w-full col-span-3 2xl:col-span-3 lg:col-span-2">
+          <SideMenu />
+        </div>
+        <div className="w-full col-span-5 xl:col-span-4 2xl:col-span-3 mt-28">
+          {children}
+        </div>
       </div>
-    </Flex>
+    </div>
   );
 };
 
