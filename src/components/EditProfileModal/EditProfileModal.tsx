a5e7ee2a34be005e8db4fbaa0b9cdb4c697807a2
Rakshit Arora <rakshit087@gmail.com>
2022-12-31 08:54:18 +0530

    polished some ui

--- src/components/EditProfileModal/EditProfileModal.tsx
+++ src/components/EditProfileModal/EditProfileModal.tsx
@@ -1,130 +1,98 @@
+import { Button, Input, Modal } from "antd";
 import React, { useState } from "react";
-import {
-  Box,
-  Button,
-  Text,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalBody,
-  Input,
-  Flex,
-  Textarea,
-  FormControl,
-} from "@chakra-ui/react";
-
-import { ChannelType } from "../../models";
 
 export interface CreateChannelModalProps {
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
-  onSubmit: (name: string, bio: string, pfp: string)=> Promise<void>
+  onSubmit: (name: string, bio: string, pfp: string) => Promise<void>;
 }
 
 export const EditProfileModal: React.FC<CreateChannelModalProps> = ({
   isOpen,
   onOpen,
   onClose,
-  onSubmit
+  onSubmit,
 }) => {
   const [linkToPfp, setLinkToPfp] = useState("");
   const [userName, setUserName] = useState("");
   const [bio, setBio] = useState("");
   const [loading, setLoading] = useState(false);
 
-
-  const editProfile = async (e : any) => {
-    e.preventDefault()
-    setLoading(true)
+  const editProfile = async (e: any) => {
+    e.preventDefault();
+    setLoading(true);
     await onSubmit(userName, bio, linkToPfp);
-    onClose()
+    onClose();
     setLoading(false);
+  };
 
-  }
+  const TextArea = Input.TextArea;
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} isCentered size={"lg"}>
-      <ModalOverlay backdropFilter="blur(10px)" />
-      <ModalContent>
-        <ModalBody py={8}>
-          <Box textAlign={"center"} mb={8}>
-            <Text fontSize={"2xl"} color={"#69248A"} fontWeight={"semibold"}>
-              Edit Profile
-            </Text>
-          </Box>
-          <Text mb={1} fontSize={"xs"} fontWeight={"semibold"}>
-            LINK TO PFP
-          </Text>
-          <form onSubmit={editProfile}>
-            <FormControl>
-              <Flex alignItems={"center"} mb={8}>
-              <Input
-                placeholder="Add pfp link"
-                value={linkToPfp}
-                onChange={(e) => setLinkToPfp(e.target.value)}
-                rounded={"sm"}
-                boxShadow={"0px 0px 20px 1px rgba(175, 92, 214, 0.25)"}
-                focusBorderColor={"#69248A"}
-                mr={4}
-              />
-            </Flex>
-            <Text mb={1} fontSize={"xs"} fontWeight={"semibold"}>
-              USERNAME
-            </Text>
-            <Flex alignItems={"center"} mb={8}>
-              <Input
-                placeholder="Add username"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-                rounded={"sm"}
-                boxShadow={"0px 0px 20px 1px rgba(175, 92, 214, 0.25)"}
-                focusBorderColor={"#69248A"}
-                mr={4}
-              />
-            </Flex>
-            <Text mb={1} fontSize={"xs"} fontWeight={"semibold"}>
-              BIO
-            </Text>
-            <Flex alignItems={"center"} mb={8}>
-              <Textarea
-                placeholder="Add bio"
-                value={bio}
-                onChange={(e) => setBio(e.target.value)}
-                rounded={"sm"}
-                boxShadow={"0px 0px 20px 1px rgba(175, 92, 214, 0.25)"}
-                focusBorderColor={"#69248A"}
-                mr={4}
-                resize={"none"}
-                height={48}
-              />
-            </Flex>
+    <Modal
+      open={isOpen}
+      onCancel={onClose}
+      centered
+      width={512}
+      wrapClassName="bg-[#F2F2F2] bg-opacity-50 backdrop-filter backdrop-blur-sm"
+      footer
+    >
+      <div>
+        <div className="mb-8 text-center">
+          <p className="text-2xl text-[#69248A] font-semibold">Edit Profile</p>
+        </div>
+        <p className="mb-1 text-xs font-semibold">LINK TO PFP</p>
+        <form onSubmit={editProfile}>
+          <Input
+            placeholder="Add pfp link"
+            value={linkToPfp}
+            onChange={(e) => setLinkToPfp(e.target.value)}
+            size="large"
+            className="mb-8 mr-4 rounded-sm"
+            style={{
+              boxShadow: "0px 0px 20px 1px rgba(175, 92, 214, 0.25)",
+            }}
+          />
 
+          <p className="mb-1 text-xs font-semibold">USERNAME</p>
+          <Input
+            placeholder="Add username"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            className="mb-8 mr-4 rounded-sm"
+            size="large"
+            style={{
+              boxShadow: "0px 0px 20px 1px rgba(175, 92, 214, 0.25)",
+            }}
+          />
+
+          <p className="mb-1 text-xs font-semibold">BIO</p>
+          <TextArea
+            placeholder="Add bio"
+            value={bio}
+            onChange={(e) => setBio(e.target.value)}
+            className="mb-8 rounded-md resize-none min-h-[200px]"
+            style={{
+              boxShadow: "0px 0px 20px 1px rgba(175, 92, 214, 0.25)",
+            }}
+            required={true}
+            autoSize={{ minRows: 8, maxRows: 12 }}
+          />
+
+          <div className="w-full text-center">
             <Button
-              isLoading={loading}
-              type="submit"
-              display={"block"}
-              mx={"auto"}
-              bg={
-                "linear-gradient(90deg, rgba(239, 136, 210, 1), rgba(175, 92, 214, 1))"
-              }
-              color={"white"}
-              rounded={"md"}
-              w={48}
-              h={10}
-              _hover={{
-                bg: "linear-gradient(90deg, rgba(239, 136, 210, 1), rgba(175, 92, 214, 1))",
-              }}
-              _active={{
-                bg: "linear-gradient(90deg, rgba(239, 136, 210, 1), rgba(175, 92, 214, 1))",
-              }}
+              type={"primary"}
+              size={"large"}
+              className="text-[24px] bg-gradient-to-r from-[#EF88D2] to-[#AF5CD6] border-none w-48 mx-auto"
+              htmlType="submit"
+              loading={loading}
             >
               Save Changes
             </Button>
-            </FormControl>
-          </form>
-        </ModalBody>
-      </ModalContent>
+          </div>
+        </form>
+      </div>
     </Modal>
   );
 };
